e226a87507e35357509c0f61205f589059d25cc1
yune06 <yune200611@gmail.com>
2024-11-11 14:04:22 +0900

    점심시간 전 백업

--- js/main.js
+++ js/main.js
@@ -4,8 +4,59 @@ $(function () {
     //header
     tl.from(".logo", { y: -100, autoAlpha: 0 });
     tl.from(".gnb li > a", { y: -50, autoAlpha: 0, stagger: 0.1 }, ">");
-    tl.from(".util-menu a", { y: -100, autoAlpha: 0, stagger: 0.35 }, "-=0.4");
+    tl.from(".util-menu a", { y: -100, autoAlpha: 0, stagger: 0.2 }, ">");
 
-    tl.from(".pick-slider-wrap", { x: 50, autoAlpha: 0 }, ">");
-    tl.from(".product-con", { y: 50, autoAlpha: 0 }, ">");
+    //pick
+    const pickTL = gsap.timeline({
+        scrollTrigger: {
+            trigger: ".pick",
+            // markers: true,
+            start: "top 10%",
+            end: "bottom 0%",
+            pin: true,
+            scrub: 1, // 사용자의 스크로바를 따라잡기까지 1초
+        },
+    });
+
+    pickTL.from(".pick-slider-wrap", {
+        autoAlpha: 0,
+        x: 50,
+        stagger: 0.2,
+    });
+
+    //news
+    const newsTL = gsap.timeline({
+        scrollTrigger: {
+            trigger: ".news",
+            // markers: true,
+            start: "top 10%",
+            end: "bottom 0%",
+            pin: true,
+            scrub: 1, // 사용자의 스크로바를 따라잡기까지 1초
+        },
+    });
+
+    newsTL.from(".swiper-wrapper a", {
+        autoAlpha: 0,
+        y: 50,
+
+        stagger: 0.25,
+    });
+
+    //service
+    const serviceTL = gsap.timeline({
+        scrollTrigger: {
+            trigger: ".service",
+            // markers: true,
+            start: "top 10%",
+            end: "bottom 0%",
+            pin: true,
+            scrub: 1, // 사용자의 스크로바를 따라잡기까지 1초
+        },
+    });
+
+    serviceTL.from(".ri-arrow-right-line", {
+        autoAlpha: 0,
+        x: -50,
+    });
 });
--- js/story.js
+++ js/story.js
@@ -3,13 +3,14 @@ $(function () {
 
     //main
     tl.from(".header strong", { x: -100, autoAlpha: 0, duration: 1 });
-    tl.from(".link  li", { y: -40, autoAlpha: 0, stagger: 0.25 }, "-=0.2");
     tl.from(".section-title", { x: -100, autoAlpha: 0, duration: 1 }, ">");
-    tl.from(".list-con li", { y: -50, autoAlpha: 0, stagger: 0.15 }, "-=0.4");
+    tl.from(".link  li", { y: -40, autoAlpha: 0, stagger: 0.25 }, "-=0.2");
+    tl.from(".list-con li", { y: -50, autoAlpha: 0, stagger: 0.15 }, ">");
 
-    tl.from(".vision-con h2", { autoAlpha: 0, duration: 1 });
-    tl.from(".vision-con figure", { x: -50, autoAlpha: 0, stagger: 0.15 }, "-=0.4");
-    tl.from(".message h2", { x: -50, autoAlpha: 0, duration: 1 });
+    // tl.from(".vision-con h2", { autoAlpha: 0, duration: 1 });
+    // tl.from(".vision-con figure", { x: -50, autoAlpha: 0, stagger: 0.15 }, "-=0.4");
+    // tl.from(".message h2", { x: -50, autoAlpha: 0, duration: 1 });
+
     // tl.from(".messsage-con p", {
     //     y: -50,
     //     autoAlpha: 0,
@@ -22,4 +23,42 @@ $(function () {
     //     stagger: 1,
     //     duration: 1,
     // });
+
+    //vision
+    const visionTL = gsap.timeline({
+        scrollTrigger: {
+            trigger: ".vision",
+            markers: true,
+            start: "top 30%",
+            end: "bottom 0%",
+
+            scrub: 1, // 사용자의 스크로바를 따라잡기까지 1초
+        },
+    });
+
+    visionTL.from(".vision-con h2 ", {
+        autoAlpha: 0,
+        y: -30,
+    });
+    visionTL.from(".figure figure", {
+        autoAlpha: 0,
+        x: -30,
+    });
+
+    //message
+    const ceoMessageTL = gsap.timeline({
+        scrollTrigger: {
+            trigger: ".ceo-message",
+            markers: true,
+            start: "top 90%",
+            end: "bottom 20%",
+
+            scrub: 1, // 사용자의 스크로바를 따라잡기까지 1초
+        },
+    });
+
+    ceoMessageTL.from(".message h2 ", {
+        autoAlpha: 0,
+        x: -50,
+    });
 });
